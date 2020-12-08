8ecc66ffa05f4d9dacdd0139d2caa6bff2506489
shammaasmahmood <shammaasmahmood8110@gmail.com>
2020-12-08 16:44:08 +0500

    Assignmentjava

---
+++ Assignment.java
@@ -0,0 +1,19 @@
+import java.io.*; 
+  
+class Assignment { 
+    public static void main(String[] args) 
+    { 
+  
+        // Declaring variables 
+        int num; 
+        String name; 
+  
+        // Assigning values 
+        num = 10; 
+        name = "Pakistan"; 
+  
+        // Displaying the assigned values 
+        System.out.println("num is assigned: " + num); 
+        System.out.println("name is assigned: " + name); 
+    } 
+} 
